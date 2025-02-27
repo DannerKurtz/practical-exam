fdb0e8ddc11804945ea52aea10dcc99a1e9639b0
Danner Kurtz <kurtz.danner@gmail.com>
2025-02-27 08:18:34 -0300

    add button

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,5 +2,14 @@ import HeroSection from './hero-section';
 import ProductCard from './product-card';
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div>
+        <HeroSection src='https://random.imagecdn.app/940/300' />
+      </div>
+      <div>
+        <ProductCard></ProductCard>
+      </div>
+    </main>
+  );
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,5 +4,13 @@ import { twMerge } from 'tailwind-merge';
 interface ButtonProps extends ComponentProps<'button'> {}
 
 export default function Button({ className, ...props }: ButtonProps) {
-  return <></>;
+  return (
+    <button
+      className={twMerge(
+        'px-5 h-12 flex justify-between items-center bg-gray-100 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300',
+        className
+      )}
+      {...props}
+    />
+  );
 }
