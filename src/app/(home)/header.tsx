40bc1104ba4471a2f2a0a3bc333eb1b148bd7384
Danner Kurtz <kurtz.danner@gmail.com>
2025-02-27 19:17:03 -0300

    added navigation home and cart

--- src/app/(home)/header.tsx
+++ src/app/(home)/header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Button from '@/components/button';
 import { ShoppingCart } from 'lucide-react';
 
@@ -6,14 +7,20 @@ export default function Header() {
     <header className='bg-blue-600 h-16 flex justify-between items-center px-5'>
       <nav className='max-w-[1240px] w-full mx-auto'>
         <ul className='flex justify-between items-center'>
-          <li className='bg-gray-100 text-black inline-flex items-center space-x-1 p-2 rounded-xl'>
-            <h1 className='text-3xl font-bold'>Practical</h1>
-            <p className='text-2xl '>exam</p>
+          <li className='bg-gray-100 text-black inline-flex items-center space-x-1 p-2 rounded-xl cursor-pointer'>
+            <Link href='/'>
+              <div className='flex items-center'>
+                <h1 className='text-3xl font-bold'>Practical</h1>
+                <p className='text-2xl'>exam</p>
+              </div>
+            </Link>
           </li>
           <li className='pr-5'>
-            <Button>
-              <ShoppingCart className='text-black' />
-            </Button>
+            <Link href='/cart'>
+              <Button>
+                <ShoppingCart className='text-black' />
+              </Button>
+            </Link>
           </li>
         </ul>
       </nav>
