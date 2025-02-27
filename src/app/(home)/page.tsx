810b07c8ff3cd124b50025d5cb7fc39e71216248
Danner Kurtz <kurtz.danner@gmail.com>
2025-02-27 05:20:27 -0300

    refactor: move product-card component to home-page folder
    The product-card component is specific to the products page.
    It has been moved from the global components folder to the home-page folder.

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import ProductCard from './product-card';
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        {/* <ProductCard /> */}
+      </div>
+    </main>
+  );
+}
