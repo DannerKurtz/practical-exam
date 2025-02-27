c0df4c24953ecad2b81cf3be02756b755f8c907e
Danner Kurtz <kurtz.danner@gmail.com>
2025-02-27 07:34:02 -0300

    feat: add Header component
    The Header component has been created and includes a logo, navigation, and a shopping cart icon. It is styled using Tailwind CSS and designed to be responsive.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,7 +6,7 @@ export default function Button(props: ButtonProps) {
   return (
     <>
       <button
-        className='px-5 h-12 flex justify-between items-center bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900'
+        className='px-5 h-12 flex justify-between items-center bg-gray-100 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900'
         {...props}
       />
     </>
