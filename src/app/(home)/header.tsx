16ae43bb5e71379d38c77c96310d31f8785afc1a
Danner Kurtz <kurtz.danner@gmail.com>
2025-02-27 21:16:24 -0300

    .

--- src/app/(home)/header.tsx
+++ src/app/(home)/header.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import Button from '@/components/button';
 import { ShoppingCart } from 'lucide-react';
 
@@ -7,20 +6,14 @@ export default function Header() {
     <header className='bg-blue-600 h-16 flex justify-between items-center px-5'>
       <nav className='max-w-[1240px] w-full mx-auto'>
         <ul className='flex justify-between items-center'>
-          <li className='bg-gray-100 text-black inline-flex items-center space-x-1 p-2 rounded-xl cursor-pointer'>
-            <Link href='/'>
-              <div className='flex items-center'>
-                <h1 className='text-3xl font-bold'>Practical</h1>
-                <p className='text-2xl'>exam</p>
-              </div>
-            </Link>
+          <li className='bg-gray-100 text-black inline-flex items-center space-x-1 p-2 rounded-xl'>
+            <h1 className='text-3xl font-bold'>Practical</h1>
+            <p className='text-2xl '>exam</p>
           </li>
           <li className='pr-5'>
-            <Link href='/cart'>
-              <Button>
-                <ShoppingCart className='text-black' />
-              </Button>
-            </Link>
+            <Button>
+              <ShoppingCart className='text-black' />
+            </Button>
           </li>
         </ul>
       </nav>
